79647a11a508a1cf85748deadea21c90908715a9
Rahul Mondal <57228346+clumsyspeedboat@users.noreply.github.com>
2021-02-03 13:53:28 +0100

    Update [Clustering + Ordination](K-Means+PCA).R

--- Understand_R_Basics/[Clustering + Ordination](K-Means+PCA).R
+++ Understand_R_Basics/[Clustering + Ordination](K-Means+PCA).R
@@ -1,10 +1,9 @@
-
 ##############################
 ## Clustering + Ordination ###
 ##############################
 
 
-# Cleaning the workspace
+# Cleaning the workplace
 
 cat("\f")       # Clear old outputs
 rm(list=ls())   # Clear all variables
@@ -22,9 +21,11 @@ data_matrix <- read.csv(data_csv, header = TRUE, sep = ",")
 
 # Installing  Packages
 if(!require("factoextra")) install.packages("factoextra")   # For Visualization
+if(!require("NbClust")) install.packages("NbClust")   # For Visualization
 
 # Loading Package
 library("factoextra")
+library("NbClust")
 
 
 
@@ -35,12 +36,29 @@ pca <- prcomp(data_matrix,scale=T,center = T)
 comp <- data.frame(pca$x[,1:2])
 comp
 
-
 # Plot the result
 plot(comp, pch=16)
 
+
+
 # Applying K-Means
-k_means_new <- eclust(comp, FUNcluster= "kmeans", k= 3, hc_metric = "euclidean" , nstart = 25, graph = TRUE)
+
+# Determining the optimum number of clusters 
+nb <- NbClust(data_matrix, distance = "euclidean", min.nc = 2,
+              max.nc = 5, method = "complete", index ="all")
+
+
+# Visualize the result
+
+fviz_nbclust(nb) + theme_minimal()
+
+
+# Input desired number of clusters
+
+K <- readline(prompt = "Input number of clusters required:")
+k <- as.integer(K)
+
+k_means_new <- eclust(comp, FUNcluster= "kmeans", k, hc_metric = "euclidean" , nstart = 25, graph = TRUE)
 
 
 ####################################################################################

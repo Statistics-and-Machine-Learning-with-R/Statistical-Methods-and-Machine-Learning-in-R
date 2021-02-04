4cbc0dfdbe003d825faf2a8850df0d672ca615c7
Rahul Mondal <57228346+clumsyspeedboat@users.noreply.github.com>
2021-02-04 19:18:43 +0100

    Update [Clustering + Ordination](K-Means+PCA).R

--- Understand_R_Basics/[Clustering + Ordination](K-Means+PCA).R
+++ Understand_R_Basics/[Clustering + Ordination](K-Means+PCA).R
@@ -2,13 +2,18 @@
 ## Clustering + Ordination ###
 ##############################
 
-
-# Cleaning the workplace
-
 cat("\f")       # Clear old outputs
 rm(list=ls())   # Clear all variables
 
-# Creating a data matrix
+# Please make sure your csv file contains only numeric variables for the code to run.
+
+# If your csv file has non numeric variables, make sure to remove them or alternatively you can choose a subset of 
+# your data at " # Plotting QQ Plots " below
+
+# To run the code, select the whole code and run as source (top right in this window) & enter parameter values in the console below
+
+
+# Loading data set
 
 data_csv <- file.choose()
 data_matrix <- read.csv(data_csv, header = TRUE, sep = ",")
@@ -59,6 +64,5 @@ K <- readline(prompt = "Input number of clusters required:")
 k <- as.integer(K)
 
 k_means_new <- eclust(comp, FUNcluster= "kmeans", k, hc_metric = "euclidean" , nstart = 25, graph = TRUE)
-
 
 ####################################################################################

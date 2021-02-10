8d997f5b09234b65bc8e6af997c6a7dc07ece189
Faizan Ali <49519053+faizan8201@users.noreply.github.com>
2021-02-10 12:45:01 +0100

    Update Ordination_cca.R

--- Unsupervised Learning/Ordination_cca.R
+++ Unsupervised Learning/Ordination_cca.R
@@ -3,6 +3,19 @@
    one column with CATEGORICAL variables (for sample check the dataset provided with the 
    name ' German_State_Results').
 
+                      Column(Variable) 1      Column(Variable) 2     . . . .    Column(Variable) n
+      
+      Row(Instance) 1      (Value)                  (Value)           . . . .         (Value)
+      
+      Row(Instance) 2      (Value)                  (Value)           . . . .         (Value)
+      
+      .                       .                        .                                 .
+      .                       .                        .                                 .
+      .                       .                        .                                 .
+      .                       .                        .                                 .
+      
+      Row(Instance) n      (Value)                  (Value)           . . . .         (Value)
+
 2- To run the code, select the whole code and run as 'source with echo' (top right in this window) & enter parameters
    which will be asked on running the code in the CONSOLE screen. In this case select:
    

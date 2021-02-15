b80395326d83e6680c5a2114b233b2f3c766f1e7
Rizvix0 <49483578+Rizvix0@users.noreply.github.com>
2021-02-15 18:29:07 +0100

    Update Anosim_and_Permanova.R

--- Significant Difference Tests/ANOSIM_test & PERMANOVA_test/Anosim_and_Permanova.R
+++ Significant Difference Tests/ANOSIM_test & PERMANOVA_test/Anosim_and_Permanova.R
@@ -35,64 +35,58 @@
   library("miniCRAN")
   library("ggpubr")
   library("vegan")
-  library("vctrs")
-  
+
   cat("\f")       # Clear old outputs
   
-  # Class for the input file
-  setClass(Class="CSV_data",
-           representation(
-             MATRIX="data.frame",
-             VALUES="data.frame",
-             RNames = "character",
-             CNames = "character"
-           )
-  )
+  #------------------------------------------------
+  "SELECTION OF DATASETS AND PARAMETERS"
+  #------------------------------------------------
   
   
-  #------------------------------------------------
-  "Inputting the matrix"
-  #------------------------------------------------
+  print(paste("Please select Input CSV", " The different samples in columns and the measured variables in the rows.
+              Here we choose our Input file: Input_file_Anosim&Permanova.csv"), quote = FALSE)
+  fname <- file.choose()
   
   #Choose the Separator for file
   ask_sep <- as.character(readline(prompt = " ENTER the SEPARATOR for file(',' or ';') : "))
   
-  print(paste("Please select Input CSV", " The different samples in columns and the measured variables in the rows.
-              Here we choose our Input file: Input_file_Anosim&Permanova.csv"), quote = FALSE)
+  matrix <- read.csv(fname, sep=ask_sep, row.names=1)  
   
-  matrix <- read.csv(file.choose(), sep = ask_sep, row.names=1)
-  values <- matrix[2:nrow(matrix),2:ncol(matrix)]
-  rNames <- row.names(matrix)
-  cNames <- colnames(matrix)
-  fname  <- new("CSV_data", MATRIX = matrix, VALUES = values, RNames = rNames, CNames = cNames )
-  NORMDATA <- fname
+  #Extract continuous variables:
+  start_row   <- as.integer(readline(prompt = "Enter value for START of range of numerical values in rows: "))
+  cat("\f")     #Clear old outputs
+  start_col   <- as.integer(readline(prompt = "Enter value for START of range of numerical values in columns: "))
   
-  #------------------------------------------------
-  "Load Classifications"
-  #------------------------------------------------
+  values <- matrix[start_row:nrow(matrix),start_col:ncol(matrix)]
+  NORMDATA <- as.matrix(values)
   
   #Now, Selecting the grouping file.
   print(paste("Please select Grouping CSV", "Care about correct format."), quote = FALSE)
-  fname <- file.choose()    # select 'Groupings_file_Anosim&Permanova'
-  groups <- as.matrix(read.csv(fname, sep=ask_sep,row.names=1))
+  fname1 <- file.choose()    # select 'Groupings_file_Anosim&Permanova'
+  groups <- as.matrix(read.csv(fname1, sep=ask_sep,row.names=1))
   
   #------------------------------------------------
-  "Applying significance tests for groups"
+  "Calculation for Anosim and Permanova"
   #------------------------------------------------
   
   #Select ANOSIM or PERMANOVA Test:
   cat("\f")       # Clear old outputs
   groupSimMethod <- readline(prompt = "Write 'a' for Anosim or 'p' for Permanova: ")
+    if(groupSimMethod== "a"){
+      cat("\f")       # Clear old outputs
+      #Select Choice of distance metric for Anosim
+      ask_dist <- readline(prompt = "Write your Choice of distance metric between manhattan,euclidean,bray,jaccard:")
+      
+    }
   
   #Select the number of Permutations you want. Permutations are for shuffling the data in groups
-  perm  <- readline(prompt = "Write the number of Permutations you want:")
+  perm <- as.numeric(readline(prompt = "Write the number of Permutations you want:"))
   
     #Get the data matrix
-    groupMatrix <- NORMDATA@MATRIX
+    groupMatrix <- NORMDATA
     
     #Get non-redundant list of subgroups
     uniqueGroups <- unique(t(groups))
-    
     #Creates the output matrix for R and p-values
     groupOutputMatrixR <- matrix("empty", nrow=nrow(uniqueGroups), ncol= nrow(uniqueGroups))
     groupOutputMatrixp <- matrix("empty", nrow=nrow(uniqueGroups), ncol= nrow(uniqueGroups))
@@ -106,7 +100,7 @@
     for (i in 1:nrow(uniqueGroups)){
       for (j in 1:nrow(uniqueGroups)){
         
-        #Create submatrix and its classification subvextor
+        #Create submatrix
         #Empty subgroupmatrix
         subgroupMatrix = matrix(nrow =nrow(groupMatrix), ncol = 0)
         #Set the rownames
@@ -127,40 +121,27 @@
         }
         
         #A P value less than 0.05 is generally considered to be statistically significant, 
-        #An R value close to "1.0" means dissimilarity between groups 
-        #while an R value close to "0" suggests an even distribution of high and low ranks within and between groups
-        if(uniqueGroups[i,1] == uniqueGroups[j,1]){
+        #An R value close to “1.0” means dissimilarity between groups 
+        #while an R value close to “0” suggests an even distribution of high and low ranks within and between groups
+         if(uniqueGroups[i,1] == uniqueGroups[j,1]){
           #Get R value
           groupOutputMatrixR[i,j] <-1
+          
           #Get p-value
-          groupOutputMatrixp[i,j] <- 0
+          groupOutputMatrixp[i,j] <-0
           
         }else{
-          #Calculate ANOSIM or PermANOVA
+          #Calculate Anosim or Permanova
           if (groupSimMethod =="a") {
-            #Perform ANOSIM
-            res <- anosim(t(subgroupMatrix), classifications, permutations = perm, distance = "manhattan", strata = NULL,
+            #Perform Anosim
+            res <- anosim(t(subgroupMatrix), classifications, permutations = perm, ask_dist, strata = NULL,
                           parallel = getOption("mc.cores"))
             
             #Get R value
             groupOutputMatrixR[i,j] <- res$statistic
             #Get p-value
             groupOutputMatrixp[i,j] <- res$signif
-            
-            #--------------------------------------------------
-            "Export the results"
-            #--------------------------------------------------
-            
-            write.table(groupOutputMatrixp, file = paste("_pvalue_Anosim",groupSimMethod,".csv"), append = FALSE, quote = TRUE, sep = ask_sep,
-                        eol = "\n", na = "NA", dec = ".", row.names = TRUE,
-                        col.names = NA, qmethod = c("escape", "double"),
-                        fileEncoding = "")
-            write.table(groupOutputMatrixR, file = paste("_Rvalue_Anosim",groupSimMethod,".csv"), append = FALSE, quote = TRUE, sep = ask_sep,
-                        eol = "\n", na = "NA", dec = ".", row.names = TRUE,
-                        col.names = NA, qmethod = c("escape", "double"),
-                        fileEncoding = "")
-            
-            
+    
           }else if (groupSimMethod =="p"){
             # Perform Permanova
             res2 <- adonis(t(subgroupMatrix) ~ CLASSIFICATIONS, data =as.data.frame(t(classifications)), permutations=perm)
@@ -170,25 +151,25 @@
             #Get p-value
             groupOutputMatrixp[i,j] <- res2$aov.tab$`Pr(>F)`[1]
             
-            #--------------------------------------------------
-            "Export the results"
-            #--------------------------------------------------
-            
-            write.table(groupOutputMatrixp, file = paste("_PValue_Permanova",groupSimMethod,".csv"), append = FALSE, quote = TRUE, sep = ask_sep,
-                        eol = "\n", na = "NA", dec = ".", row.names = TRUE,
-                        col.names = NA, qmethod = c("escape", "double"),
-                        fileEncoding = "")
-            write.table(groupOutputMatrixR, file = paste("_RValue_Permanova",groupSimMethod,".csv"), append = FALSE, quote = TRUE, sep = ask_sep,
-                        eol = "\n", na = "NA", dec = ".", row.names = TRUE,
-                        col.names = NA, qmethod = c("escape", "double"),
-                        fileEncoding = "")
           }
         }
       }
-    
-    
-    
-      print(paste("FINISHED"), quote = FALSE)
-      cat("\f")       # Clear old outputs
     }
+    
+  #--------------------------------------------------
+  "Export the results"
+  #--------------------------------------------------  
+    
+    write.table(groupOutputMatrixp, file = paste("Pvalue",".csv"), append = FALSE, quote = TRUE, sep = ask_sep,
+                eol = "\n", na = "NA", dec = ".", row.names = TRUE,
+                col.names = NA, qmethod = c("escape", "double"),
+                fileEncoding = "")
+    write.table(groupOutputMatrixR, file = paste("Rvalue",".csv"), append = FALSE, quote = TRUE, sep = ask_sep,
+                eol = "\n", na = "NA", dec = ".", row.names = TRUE,
+                col.names = NA, qmethod = c("escape", "double"),
+                fileEncoding = "")
+    
+    
+  print(paste("FINISHED"), quote = FALSE)
+  cat("\f")       # Clear old outputs
     

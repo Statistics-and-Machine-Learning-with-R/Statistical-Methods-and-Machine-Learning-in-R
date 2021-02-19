1f8449524a49b86e15a721f035a0322c516b2de7
Faizan Ali <49519053+faizan8201@users.noreply.github.com>
2021-02-19 17:14:45 +0100

    Update Ordination_cca.R

--- Unsupervised Learning/Ordination_cca.R
+++ Unsupervised Learning/Ordination_cca.R
@@ -1,22 +1,24 @@
 "
 NOTE: First Column is treated as 1 in the Selection of Data:
 
-1- Please make sure your csv file contains  NUMERIC variables with headers for the code and  
+1- Please make sure your csv file contains  NUMERIC variables with headers and  
    one column with CATEGORICAL variables (for sample check the dataset provided with the 
-   name ' German_State_Results').
-
-                      Column(Variable) 1      Column(Variable) 2     . . . .    Column(Variable) n
+   name ' German_State_Results', In this data Categorical variable 'State' 1,2,3 represent 
+   'Berlin', 'NRW' and 'Bavaria' respectively. For Categorical variable 'Wealth' 1,2,3 represent 
+   'poor', 'middle' and 'rich' ). First row is index name of rows.
+   
+                   Column(Instance) 1      Column(Instance) 2         . . . .    Column(Instance) n
       
-      Row(Instance) 1      (Value)                  (Value)           . . . .         (Value)
+      Row(Variable) 1      (Value)                  (Value)           . . . .         (Value)
       
-      Row(Instance) 2      (Value)                  (Value)           . . . .         (Value)
+      Row(Variable) 2      (Value)                  (Value)           . . . .         (Value)
       
       .                       .                        .                                 .
       .                       .                        .                                 .
       .                       .                        .                                 .
       .                       .                        .                                 .
       
-      Row(Instance) n      (Value)                  (Value)           . . . .         (Value)
+      Row(Variable) n      (Value)                  (Value)           . . . .         (Value)
 
 2- To run the code, select the whole code and run as 'source with echo' (top right in this window) & enter parameters
    which will be asked on running the code in the CONSOLE screen. In this case select:
@@ -61,12 +63,13 @@ outputname <- 'Results_of_cca'
 print(paste("Please select Input CSV", " The different samples in columns and the measured variables in the rows."), quote = FALSE)
 fname <- file.choose()
 
-#ask user for type of  Separator:
-ask_sep <- as.character(readline(prompt = "ENTER either of the types of Separator ',' or ';' : "))
-
-#Input file read
-file1 <- read.csv(fname, sep=ask_sep)
+#Choose the Separator for file
+ask_sep <- as.character(readline(prompt = "ENTER the SEPARATOR for file(',' or ';') : "))
+file1 <- read.csv(fname, sep = ask_sep, row.names = 1)
 cat("\f")       # Clear old outputs
+
+#Transpose of data for ecological data
+#file2 <- t(file1)
 
 #List for Start of the groups:
 group_start <- list()
@@ -87,24 +90,26 @@ cat("\f")       # Clear old outputs
 "CCA RESULTS"
 #------------------------------------------------
 #Sub space the numeric matrix
+
+file2 <- t(file1)
+
 #One group with some variables and Scalled
-group_1 <- scale(file1[,group_start[[1]] : group_end[[1]]])
+group_1 <- scale(file2[,group_start[[1]] : group_end[[1]]])
 
 #Second group with some selected variables and Scalled
-group_2 <- scale(file1[,group_start[[2]] : group_end[[2]]])
+group_2 <- scale(file2[,group_start[[2]] : group_end[[2]]])
 
 "
 CCA aims to find the associations between two data matrices (two sets of variables) X and Y.
 CCA’s goal is to find the linear projection of the first data matrix that is maximally correlated 
 with the linear projection of the second data matrix.
-
 To perform classical CCA, we use cancor() function CCA R package. 
 cancor() function computes canonical covariates between two input data matrices. 
 By default cancor() centers the columns of data matrices.
-
 "
 #applt CCA analysis
 cca <- cancor(group_1, group_2)
+
 
 "
 We can use our data Group-1 & Group-2 and the corresponding coefficients to get the canonical 
@@ -120,21 +125,27 @@ canonical_covariate_group_2 <- as.matrix(group_2) %*% cca$ycoef[, 1]
 #Corelarion among group-1 and group2
 cor(canonical_covariate_group_1,canonical_covariate_group_2)
 
+canonical_covariate_group_1
+
 #Results of CCA Analysis (Respective canonical covariate pairs)
-cca_results <-  mutate(.data = file1, canonical_covariate_group_1, canonical_covariate_group_2)
+file2 <- as.data.frame(file2)
+cca_results <-  mutate(.data = file2, canonical_covariate_group_1, canonical_covariate_group_2)
 
 #Extract the Cateogrical column and later on use in Visualization
-Cateogry <- cca_results[[ask_attr]]
+Cateogry <- as.factor(cca_results[[ask_attr]])
+
+# Extract name of the Category
+cat_name <- colnames(file2[ask_attr])
 
 #Canonical Covariate of Group_1 vs Cateogries(Species)
-ggplot(data = cca_results, aes(x=Cateogry,y=canonical_covariate_group_1, color= Cateogry))+geom_point()
+ggplot(data = cca_results, aes(x=Cateogry,y=canonical_covariate_group_1, color= Cateogry ))+geom_point() + labs(color=cat_name)  
 
 #Canonical Covariate of Group_2 vs Cateogries(Species)
-ggplot(data = cca_results, aes(x=Cateogry,y=canonical_covariate_group_2, color= Cateogry))+geom_point()
+ggplot(data = cca_results, aes(x=Cateogry,y=canonical_covariate_group_2, color= Cateogry))+geom_point()+ labs(color=cat_name)
 
 
 #Canonical Covariate of Group_1 vs Canonical Covariate of Group_2
-ggplot(data = cca_results, aes(x=canonical_covariate_group_1,y=canonical_covariate_group_2, color= Cateogry))+geom_point()
+ggplot(data = cca_results, aes(x=canonical_covariate_group_1,y=canonical_covariate_group_2, color= Cateogry))+geom_point()+ labs(color=cat_name)
 
 cat("\f")       # Clear old outputs
 #--------------------------------------------------
@@ -148,10 +159,3 @@ write.table(cca_results, file = paste(outputname), append = FALSE, quote = TRUE,
 
 cat("\f")       # Clear old outputs
 print(paste("FINISHED"), quote = FALSE)
-
-
-
-
-
-
-

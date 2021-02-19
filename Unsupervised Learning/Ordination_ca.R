2bf1892b3682b8389c427251bb9fae9f572b6c5f
Faizan Ali <49519053+faizan8201@users.noreply.github.com>
2021-02-19 18:18:17 +0100

    Update Ordination_ca.R

--- Unsupervised Learning/Ordination_ca.R
+++ Unsupervised Learning/Ordination_ca.R
@@ -1,62 +1,58 @@
 "
 NOTE: First Column is treated as 1 in the Selection of Data:
 
-1- Please make sure your csv file contains  NUMERIC variables . for example (German_State_Results) datset provided in the main folder.
-                   
-                     Column(Variable) 1      Column(Variable) 2     . . . .    Column(Variable) n
+1- Please make sure your csv file contains  NUMERIC variables. (for sample check the dataset provided with the 
+   name ' German_State_Results')
+   
+                   Column(Instance) 1      Column(Instance) 2         . . . .    Column(Instance) n
       
-      Row(Instance) 1      (Value)                  (Value)           . . . .         (Value)
+      Row(Variable) 1      (Value)                  (Value)           . . . .         (Value)
       
-      Row(Instance) 2      (Value)                  (Value)           . . . .         (Value)
+      Row(Variable) 2      (Value)                  (Value)           . . . .         (Value)
       
       .                       .                        .                                 .
       .                       .                        .                                 .
       .                       .                        .                                 .
       .                       .                        .                                 .
       
-      Row(Instance) n      (Value)                  (Value)           . . . .         (Value)
+      Row(Variable) n      (Value)                  (Value)           . . . .         (Value)
+      
 2- To run the code, select the whole code and run as 'source with echo' (top right in this window) & enter parameters
    which will be asked on running the code in the CONSOLE screen. In this case select:
    
    a- dataset to work on (after screen pops out)
-   b- Type of Separator for input and output file
-   c- Ranges of numeric data from columns
-   d- Distance measure to be implemented ('bray', 'manhattan' or 'eucladian')
+   b- Ranges of numeric data from columns
    
 3- After providing all the parameters, the code will compute following:
-   * STRESS plot              
-   * INDIVIDUAL instances on Coordinates
-   * DISTANCE MATRIX will get saved at your current working directory into a CSV fromat
+   * SCREE plot              
+   * INDIVIDUAL instances on Principl Coordinates
+   * Bi-plot for individual and variables on PCs
 "
 #------------------------------------------------
-"REQUIRED PACKAGES FOR PCoA"
+"REQUIRED PACKAGES FOR CA"
 #------------------------------------------------
 cat("\f")       # Clear old outputs
 rm(list=ls())   # Clear all variables
 
-
 # Installing  Packages
 # Package for package administration:
-if(!require("vegan")) install.packages("vegan")
-if(!require("ape")) install.packages("ape",repo='https://mac.R-project.org',dependencies = F)
+if(!require("factoextra")) install.packages("factoextra") 
+if(!require("FactoMineR")) install.packages("FactoMineR")
 
 # Add the associated libraries to the programm
-library("vegan")
-library("ape")
+library("factoextra")
+library("FactoMineR")
 
 cat("\f")       # Clear old outputs
 #------------------------------------------------
 "SELECTION OF DATSET AND PARAMETERS"
 #------------------------------------------------
-#Output file name:
-outputname <- 'Distance_matrix_NMDS'
-
 #User input for data
 print(paste("Please select Input CSV", " The different samples in columns and the measured variables in the rows."), quote = FALSE)
 fname <- file.choose()
+
 #Choose the Separator for file
 ask_sep <- as.character(readline(prompt = "ENTER the SEPARATOR for file(',' or ';') : "))
-
 file1 <- read.csv(fname, sep = ask_sep, row.names = 1)
 cat("\f")       # Clear old outputs
 
@@ -71,46 +67,22 @@ end_num <- as.integer(readline(prompt = "Enter value for END of range of numeric
 matrix <- file2[,start_num : end_num] #all cont. variables
 cat("\f")       # Clear old outputs
 
-#ask user for type of DISTANCE measure:
-ask_dist <- as.character(readline(prompt = "ENTER either of the  DISTANCE meansure 'manhattan' or 'euclidean' or 'bray' : "))
-
 #------------------------------------------------
-"NMDS RESULTS"
+"CA RESULTS"
 #------------------------------------------------
-"NMDS (Non-metric Multidimensional Scaling)"
-#calculate the distance matrix
-dist = vegdist(matrix, method = ask_dist)
+#apply CA analysis on the matrix
+ca <- CA(matrix, graph = T)
 
-#reshape distance matrix into viewbale form
-dist_matrix <- as.matrix(dist, labels = T)
+ca_eig <- get_eigenvalue(ca)
 
+fviz_screeplot(ca) + geom_hline(yintercept=33.33, linetype=0, color="red")
+row <- get_ca_row(ca)
 
-# apply NMDS on matrix, metaMDS is included in package VEGAN which performs NMDS :
-nmds <- metaMDS(dist_matrix,
-                distance = ask_dist,
-                k = 3,
-                maxit = 999, 
-                trymax = 500,
-                wascores = T)
+#individual instances plot on PCs
+fviz_ca_row(ca) 
 
-# Produces a results of test statistics for goodness of fit for each point
-goodness(nmds) 
-
-# Stress plot
-stressplot(nmds) 
-
-
-#Individual_Instances on PCs
-print(plot(nmds, display = 'site', type = 'text'))
-
-#--------------------------------------------------
-"EXPORT THE DISTANCE MATRIX INTO CSV"
-#--------------------------------------------------
-#Write DISTANCE MATRIX into csv file at your current working directory
-write.table(dist_matrix, file = paste(outputname), append = FALSE, quote = TRUE, sep = ask_sep,
-            eol = "\n", na = "NA", dec = ".", row.names = TRUE,
-            col.names = NA, qmethod = c("escape", "double"),
-            fileEncoding = "")
+# repel= TRUE to avoid text overlapping (slow if many point)
+fviz_ca_biplot(ca, repel = F)
 
 cat("\f")       # Clear old outputs
 print(paste("FINISHED"), quote = FALSE)

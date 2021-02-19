1d452e6b7e8613bced638f8d842c05beb1b5095d
Faizan Ali <49519053+faizan8201@users.noreply.github.com>
2021-02-19 17:18:38 +0100

    Update Ordination_rda.R

--- Unsupervised Learning/Ordination_rda.R
+++ Unsupervised Learning/Ordination_rda.R
@@ -3,18 +3,20 @@ NOTE: First Column is treated as 1 in the Selection of Data:
 
 1- Please make sure your csv file contains  NUMERIC variables with headers 
    
-                    Column(Variable) 1      Column(Variable) 2     . . . .    Column(Variable) n
+   
+                   Column(Instance) 1      Column(Instance) 2         . . . .    Column(Instance) n
       
-      Row(Instance) 1      (Value)                  (Value)           . . . .         (Value)
+      Row(Variable) 1      (Value)                  (Value)           . . . .         (Value)
       
-      Row(Instance) 2      (Value)                  (Value)           . . . .         (Value)
+      Row(Variable) 2      (Value)                  (Value)           . . . .         (Value)
       
       .                       .                        .                                 .
       .                       .                        .                                 .
       .                       .                        .                                 .
       .                       .                        .                                 .
       
-      Row(Instance) n      (Value)                  (Value)           . . . .         (Value)
+      Row(Variable) n      (Value)                  (Value)           . . . .         (Value)
+      
       
 2- To run the code, select the whole code and run as 'source with echo' (top right in this window) & enter parameters
    which will be asked on running the code in the CONSOLE screen. In this case select:

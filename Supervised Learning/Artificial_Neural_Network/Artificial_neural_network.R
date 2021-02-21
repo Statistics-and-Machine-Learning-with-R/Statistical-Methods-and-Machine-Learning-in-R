360b5a2535bc3227a0dbc5098657e117a57b703f
Rizvix0 <49483578+Rizvix0@users.noreply.github.com>
2021-02-21 17:11:16 +0100

    Update Artificial_neural_network.R

--- Supervised Learning/Artificial_Neural_Network/Artificial_neural_network.R
+++ Supervised Learning/Artificial_Neural_Network/Artificial_neural_network.R
@@ -1,31 +1,34 @@
 "
 NOTE: First Column is treated as 1 in the Selection of Data:
-
-1- Please select the dataset provided with the name 'German_state_results_New') or any avilable numeric data in this form
-
-                   Column(Variable) 1      Column(Variable) 2     . . . .    Column(Variable) n
+1- Please select the dataset provided with the name 'German_state_results_New') or any numeric data available.
+                   Column(Samples) 1      Column(Variable) 2     . . . .    Column(Classification) n
       
-      Row(Instance) 1      (Value)                  (Value)           . . . .         (Value)
+      Row(Variables) 1      (Value)                  (Value)           . . . .         (Value)
       
-      Row(Instance) 2      (Value)                  (Value)           . . . .         (Value)
+      Row(Instance) 2       (Value)                  (Value)           . . . .         (Value)
       
       .                       .                        .                                 .
       .                       .                        .                                 .
       .                       .                        .                                 .
       .                       .                        .                                 .
       
-      Row(Instance) n      (Value)                  (Value)           . . . .         (Value)
+      Row(Instance) n       (Value)                  (Value)           . . . .         (Value)
+      
                                         
 2- To run the code, select the whole code and run as source (top right in this window) & enter parameters
    which will be asked on running the code in the CONSOLE screen. In this case select:
-   
+
    a- Select Dataset to work on (after screen pops out)
-   b- Select what type of activation function you want
+   b- Select Separator 
+   c- Assign the Classification column
+   d- Select the size of Training set
+   e- Select the type of Activation function you want
    
 3- After providing all the parameters, the code will compute following:
    * Computation and Visulaization of Neural Network
    * Comparison of predicted output and corresponding actual test data
    * Confusion Matrix to check false positives etc.
+
 "
 
 # Cleaning the workspace to start over
@@ -34,7 +37,7 @@ rm(list=ls())   # Clear all variables
 
 
 #------------------------------------------------
-"REQUIRED PACKAGES FOR ANN"
+"REQUIRED PACKAGES FOR Amynn"
 #------------------------------------------------
 
 #Cleaning the workplace to start over
@@ -45,79 +48,120 @@ rm(list=ls())   #Clear all variables
 if(!require("neuralnet")) install.packages("neuralnet") #For using Neural network package
 if(!require("caret")) install.packages("caret")         #For confusion matrix
 
+
 library("neuralnet")
 library("caret")
 
 #------------------------------------------------
-"SELECTION OF DATASET AND PARAMETERS"
+"SELECTION OF DATASET"
 #------------------------------------------------
 
-print(paste("Please select Input CSV"), quote = FALSE)
+# choose file
+print(paste("Please select Input CSV", " The different samples in columns and the measured variables in the rows."), quote = FALSE)
 
-#choose file
-fname <- file.choose()
 #Choose the Separator for file
-ask_sep <- as.character(readline(prompt = "ENTER the SEPARATOR for file(',' or ';') : ")) #hint ";"
-data_csv <- read.csv(fname, header = TRUE, sep = ask_sep)
-matrix <- data.matrix(data_csv)
-head(matrix)
+fname <- file.choose()     #choose German_State_Results_New.csv
 
-cat("\f")       # Clear old outputs
+#type of separator used in input data
+ask_sep <- as.character(readline(prompt = "ENTER the SEPARATOR for file(',' or ';') : "))
+
+matrix<- read.csv(fname, sep= ask_sep)
+
+#extract classification column
+output_col <- as.integer(readline(prompt = "Enter the Column number of Classification Column: "))
+
+#extract Size of Training set
+training_size <- as.integer(readline(prompt = "Enter a Percentage of training dataset: "))
 
 #taking user's input for activation function
 actfct <- as.character(readline(prompt = "Enter either of the activation functions you like to use. 'tanh' or 'logistic': "))
+
+
+#dummify the data
+dmy <- dummyVars(" ~ .", data = matrix, sep = NULL)
+dmy2 <- data.frame(predict(dmy, newdata = matrix))
+matrix <- dmy2
+
+
+#normalizing our data
+normalize <- function(x) {
+   return ((x - min(x)) / (max(x) - min(x)))
+}
+
+nor_matrix <- as.data.frame(lapply(matrix, normalize))
+
+cat("\f")       #Clear old outputs
 
 
 #------------------------------------------------
 "Train-Test Data Split"
 #------------------------------------------------
 
-n = nrow(matrix)
-smp_size <- floor(0.75 * n)
+training_size <- training_size/100   #extracting Percentage
+n = nrow(nor_matrix)
+smp_size <- floor(training_size * n) #training_size asked from the user
 index<- sample(seq_len(n),size = smp_size)
 
 #Breaking into Training and Testing Sets:
-TrainingSet <- matrix[index,] 
-TestingSet <- matrix[-index,]
+TrainingSet <- nor_matrix[index,]
+TestingSet <- nor_matrix[-index,]
 
 
 #------------------------------------------------
 "Neural Network Creation"
 #------------------------------------------------
 
+#getting formula variables:
+classification <- colnames(TrainingSet[output_col])
+rest_var <- colnames(TrainingSet[names(TrainingSet) != classification])
+
+
+#Making Dynamic formula
+rest_var  <- paste(rest_var, collapse = " + ")
+nn_formula <- as.formula(paste(classification, rest_var, sep=" ~ "))
+
+
+# Using neuralnet Function for Making the Tree
 library(neuralnet)
-nn <- neuralnet( Result == "1" ~ Mathematics + Biology + History + Litrature + State + City + Wealth, 
-                 data=TrainingSet, hidden=c(4,2),act.fct = actfct, linear.output=F, threshold=0.01)
-nn
-nn$result.matrix
+nn <- neuralnet(formula = nn_formula, data=TrainingSet, 
+                hidden=c(2,1),act.fct = actfct, linear.output=F, threshold=0.01)
+
+#hidden:        the number of hidden neurons (vertices) in each layer
+#act.fct:       the activation function
+#linear.output: specifies the impact of the independent variables on the dependent variable('ResultPromoted', in this case) 
+#threshold:     change in error percentage during an iteration 
+#               after which no further optimization will be carried out
+
+
+#nn$result.matrix #to see the computations, un comment this line 
+
+#------------------------------------------------
+"Plot Neural Net"
+#------------------------------------------------
+# Plotting the Neural Network
 plot(nn)
 
-#Test the resulting output
-predicted.nn <- neuralnet::compute(nn, TestingSet)
-predicted.nn = as.data.frame(predicted.nn)
 
-head(predicted.nn$net.result)  #predicted output
-head(TestingSet)                  #TestingSet, with which we will compare the predicted output
+#------------------------------------------------
+"Predict the Output"
+#------------------------------------------------
 
-predicted.nn$net.result <- sapply(predicted.nn$net.result,round,digits=0) #rounding off the predicted output
-print(predicted.nn$net.result)
+#Predicting Output
+nn.results <- predict(nn, TestingSet)
 
-#Adding and Multiplying with 2 to make all the '0's in predicted results to be '2' 
-#so to compare with '2's in the Actual result
-res <- data.frame("Predicted"=c(predicted.nn$net.result + 2 - (predicted.nn$net.result*2)), 
-                  "Actual" = c(as.data.frame(TestingSet)$Result) )
+#scaling the result back
+rounded_nn<-sapply(nn.results,round,digits=0)
+
+#taking the classification column which we asked the user initially
+classification_col <- TestingSet[,names(TestingSet) %in% classification]
+
+res <- data.frame(rounded_nn, 
+                  classification_col)
 print(res)
 
-#RMSE
-RMSE(res$Actual,res$Predicted)
-
-#------------------------------------------------
-"Confusion Matrix"
-#------------------------------------------------
-
-#A confusion matrix is used to determine the number of true and false positives 
-#generated by our predictions. 
-confusionMatrix(as.factor(res$Actual),as.factor(res$Predicted))
+#making the confusion matrix
+Conf_Matrix <- confusionMatrix(table(res$rounded_nn, res$classification_col))
+print(Conf_Matrix)
 
 
 print(paste("FINISHED"), quote = FALSE)

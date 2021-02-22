56c7430f5c7c44986574aa2ffd0b24f1d1fb1d80
Rizvix0 <49483578+Rizvix0@users.noreply.github.com>
2021-02-22 10:54:28 +0100

    Update Artificial_neural_network.R

--- Supervised Learning/Artificial_Neural_Network/Artificial_neural_network.R
+++ Supervised Learning/Artificial_Neural_Network/Artificial_neural_network.R
@@ -77,7 +77,7 @@ View(matrix)
 output_col <- as.integer(readline(prompt = "Enter the Column number of Classification Column: "))
 
 #extract Size of Training set
-training_size <- as.integer(readline(prompt = "Enter a Percentage of training dataset: "))
+training_size <- as.integer(readline(prompt = "Enter a Percentage of training dataset (e.g. 75): "))
 
 #taking user's input for activation function
 actfct <- as.character(readline(prompt = "Enter either of the activation functions you like to use. 'tanh' or 'logistic': "))
